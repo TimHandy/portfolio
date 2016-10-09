32bbd31bd8ffb0bd99c795a2ce80786757fea754
Tim Handy <tjhandy@gmail.com>
2016-10-09 17:27:22 +0100

    Add some todos

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 //TODO: #99CC33 - nice green.... or a yellow and grey? Multi-tone of same colour?
+
+// TODO: add the timhandy/github.io/projectname urls to the page. I have directly enabled github pages for each of them
+
+// TODO: add the googlehomepage copy I did.
