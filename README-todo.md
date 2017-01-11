ea4ae172267a5f87272ee96c69ad76521234adbe
Tim Handy <tjhandy@gmail.com>
2017-01-11 12:47:12 +0000

    Resolve CSS positioning

---
+++ README-todo.md
@@ -0,0 +1,6 @@
+
+TODO: add the timhandy/github.io/projectname urls to the page. I have directly enabled github pages for each of them
+
+Add a link to the git repos also
+
+TODO: add the googlehomepage copy I did.
--- main.js
+++
@@ -1,5 +0,0 @@
-//TODO: #99CC33 - nice green.... or a yellow and grey? Multi-tone of same colour?
-
-// TODO: add the timhandy/github.io/projectname urls to the page. I have directly enabled github pages for each of them
-
-// TODO: add the googlehomepage copy I did.
