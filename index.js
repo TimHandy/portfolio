a9914f967a7f7319a14526f667626bb79540ba33
Tim Handy <tjhandy@gmail.com>
2017-02-13 20:46:01 +0000

    Add repo urls to old projects

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const projects = [
     title: 'Image Search Abstraction Layer',
     img: 'http://pix.toile-libre.org/upload/thumb/1485678414.png',
     url: 'https://image-search-abst-layer.herokuapp.com/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/image-search-abstraction-layer',
     description: 'Returns paginated image search data in JSON format, and stores recent searches in a database.',
     technologies: ['Express', 'Node', 'MongoDB', 'Javascript']
   },
@@ -63,7 +63,7 @@ const projects = [
     title: 'File Metadata Microservice<br/> API CHANGED - BROKEN',
     img: 'http://pix.toile-libre.org/upload/thumb/1485678414.png',
     url: 'https://file-metadata-microsrvc.herokuapp.com/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/file-metadata-microservice',
     description: 'Choose a file to upload and it will return the file size.',
     technologies: ['Node', 'Express', 'Javascript']
   },
@@ -71,7 +71,7 @@ const projects = [
     title: 'Request Header Microservice',
     img: 'http://pix.toile-libre.org/upload/thumb/1485678414.png',
     url: 'https://req-header-service.herokuapp.com/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/request-header-parser-microservice',
     description: 'Queries the HTTP request header and sends back a JSON response containing the user\'s IP address and OS.',
     technologies: ['Node', 'Express', 'Javascript']
   },
@@ -79,7 +79,7 @@ const projects = [
     title: 'URL Shortener Microservice',
     img: 'http://pix.toile-libre.org/upload/thumb/1485678414.png',
     url: 'https://fcc-minurl.herokuapp.com/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/url-shortener-microservice',
     description: 'When using the shortened URL it checks the database and forwards the request to the original URL.',
     technologies: ['Node', 'Express', 'MongoDB', 'Javascript']
   },
@@ -87,7 +87,7 @@ const projects = [
     title: 'Timestamp Microservice',
     img: 'http://pix.toile-libre.org/upload/thumb/1485678414.png',
     url: 'https://mytimestamp-microservice.herokuapp.com/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/timestamp-microservice',
     description: 'Pass a string (unix timestamp or natural language) as a URL parameter and it will return json dates.',
     technologies: ['Node', 'Express', 'Javascript']
   },
@@ -95,31 +95,31 @@ const projects = [
     title: 'Teams Chooser',
     img: '',
     url: 'https://github.com/TimHandy/football-manager',
-    repo: '',
+    repo: 'https://github.com/TimHandy/football-manager',
     description: 'Selects equally matched teams and tracks scores across the season.',
     technologies: ['HMTL', 'CSS', 'Javascript', 'jQuery']
   },
   {
     title: 'Simon Game',
     img: 'http://pix.toile-libre.org/upload/original/1475495084.png',
-    url: 'http://codepen.io/timhandy/full/BLRzVb/',
-    repo: '',
+    url: 'https://timhandy.github.io/simon/',
+    repo: 'https://github.com/TimHandy/simon',
     description: 'Simon is an electronic game of memory skill invented in the 1970\'s.',
     technologies: ['HMTL', 'CSS', 'Javascript', 'jQuery']
   },
   {
     title: 'Pomodoro',
     img: 'http://pix.toile-libre.org/upload/original/1473167257.png',
-    url: 'http://codepen.io/timhandy/full/RGNEbv/',
-    repo: '',
+    url: 'https://timhandy.github.io/pomodoro/',
+    repo: 'https://github.com/TimHandy/pomodoro',
     description: 'A Pomodoro is a 25 min countdown timer to aid in productivity.',
     technologies: ['HMTL', 'CSS', 'Javascript', 'jQuery']
   },
   {
     title: 'Calculator',
     img: 'http://pix.toile-libre.org/upload/original/1472821840.png',
-    url: 'http://codepen.io/timhandy/full/ORJrEm/',
-    repo: '',
+    url: 'https://timhandy.github.io/calculator/',
+    repo: 'https://github.com/TimHandy/calculator',
     description: 'A calculator with all the common functionality you expect of a standard calculator.',
     technologies: ['HMTL', 'CSS', 'Javascript', 'jQuery']
   },
@@ -127,7 +127,7 @@ const projects = [
     title: 'Twitch TV<br/> API CHANGED - BROKEN',
     img: 'http://pix.toile-libre.org/upload/original/1471604448.png',
     url: 'http://codepen.io/timhandy/full/bZJWjq/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/TwitchTV',
     description: 'An application that pulls data from the Twitch API and allows toggling of visual information.',
     technologies: ['HMTL', 'CSS', 'Javascript', 'jQuery', 'API']
   },
@@ -135,7 +135,7 @@ const projects = [
     title: 'Wikipedia Viewer',
     img: 'http://pix.toile-libre.org/upload/original/1471603263.png',
     url: 'https://codepen.io/timhandy/full/GqPJyr/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/wikipedia_viewer',
     description: 'A Wikipedia viewer that will make an API call and display the data from the response.',
     technologies: ['HMTL', 'CSS', 'Javascript', 'jQuery', 'API', 'Bootstrap']
   },
@@ -143,7 +143,7 @@ const projects = [
     title: 'Local Weather App<br/> API CHANGED - BROKEN',
     img: 'http://pix.toile-libre.org/upload/original/1471603166.png',
     url: 'https://codepen.io/timhandy/full/RRJvaq/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/local_weather',
     description: 'Retrieves the local weather based on your browser geolocation data.',
     technologies: ['HMTL', 'CSS', 'Javascript', 'jQuery', 'API', 'Bootstrap']
   },
@@ -159,7 +159,7 @@ const projects = [
     title: 'Random Quotes',
     img: 'http://pix.toile-libre.org/upload/original/1471603315.png',
     url: 'https://codepen.io/timhandy/full/akGGwJ/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/random_quote_generator',
     description: 'Provides a random quote on each button press and enable sending a Tweet of the quote.',
     technologies: ['Javascript', 'HTML', 'CSS', 'Bootstrap']
   },
@@ -167,9 +167,17 @@ const projects = [
     title: 'Tribute Page',
     img: 'http://pix.toile-libre.org/upload/original/1471603398.png',
     url: 'https://codepen.io/timhandy/full/eZxGXV/',
-    repo: '',
+    repo: 'https://github.com/TimHandy/tribute_page',
     description: 'Project #1: A simple tribute page to the Extreme Sports star Travis Pastrana.',
     technologies: ['HTML', 'CSS', 'Bootstrap']
+  },
+  {
+    title: 'Google Homepage',
+    img: '',
+    url: 'http://timhandy.github.io/google-homepage/',
+    repo: 'https://github.com/TimHandy/google-homepage',
+    description: 'Basic copy of the Google homepage with HTML & CSS',
+    technologies: ['HTML', 'CSS']
   }
 ]
 
@@ -180,7 +188,6 @@ function formatProjects(projectsArr) {
   const newProjectsArr = projectsArr.map(project => {
 
     const title = project.title || 'TITLE HERE!'
-    // const img = project.img || 'http://pix.toile-libre.org/upload/thumb/1485678744.png'
     const img = project.img || './img/JavaScript-Logo.png'
     const repo = project.repo || 'https://github.com/TimHandy'
     const url = project.url || repo || 'https://github.com/TimHandy' 
